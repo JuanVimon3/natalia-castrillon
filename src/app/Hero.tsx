36800756d4c20220ff1dc5efe3461bbb6c6c9dbb
JuanVimon3 <jvillamonsalve@gmail.com>
2025-08-05 17:38:46 -0500

    add home's first section

--- src/app/Hero.tsx
+++ src/app/Hero.tsx
@@ -6,25 +6,38 @@ export default function Hero(): JSX.Element {
   return (
     <>
       <section className="bg-[#FFF9EF] py-20 px-20">
-        <h1 className="text-5xl font-bold font-display text-[#A7281B]">Natalia Castrillón</h1>
-        <h3 className="text-[#7B4812] ">Subtítulo</h3>
 
-        <div className="flex justify-center items-center gap-8 mt-10">
-          <section className="flex-1 bg-[#F2D64B] text-[#7B4812] py-20 px-10 text-center">
-            <h1 className="break-words text-justify">Lorem ipsum dolor sit amet, cghjkmnbnfghjmnbvftyujmnbvftyukmnbvfrtyukmnbvfrtyukmnbvfrtyuikmnbvftyuikmnb vcxdertyujmnbvcder56y7uikmnbvcxsertyujnbvcxdertyuikmnbvcxsertyuikmnbvcdertyujnbvcdsertyujhbvcdjhgfdcvbnkuytgfdfvbnjuytrfd</h1>
-          </section>
 
-          <section className="flex-1 text-center">
+        <div className="flex flex-col md:flex-row items-start gap-8 mt-10">
+
+          <div className="w-full md:w-2/5 flex flex-col gap-4">
+
+            <div className="flex flex-col text-left">
+              <h1 className="text-3xl md:text-5xl font-bold font-display text-[#A7281B]">Natalia Castrillón</h1>
+              <h3 className="text-[#7B4812] text-xl md:text-3xl mt-2 ">Subtítulo</h3>
+            </div>
+
+            <section>
+              <section className="bg-[#F2D64B] text-[#7B4812] py-5 px-4  text-center rounded-3xl">
+                <h1 className="break-words text-justify">Lorem ipsum dolor sit amet, cghjkmnbnfghjmnbvftyujmnbvftyukmnbvfrtyukmnbvfrtyukmnbvfrtyuikmnbvftyuikmnb vcxdertyujmnbvcder56y7uikmnbvcxsertyujnbvcxdertyuikmnbvcxsertyuikmnbvcdertyujnbvcdsertyujhbvcdjhgfdcvbnkuytgfdfvbnjuytrfd</h1>
+              </section>
+            </section>
+
+          </div>
+
+
+           <section className="w-full md:flex-1 text-center">
             <Image
               src="/principalImages/foto1.jpg"
               alt="Imagen de portada"
-              width={5000}
-              height={300}
+              width={900}
+              height={600}
             />
-          </section>         
+          </section>  
+
         </div>
 
-        
+
 
       </section>
     </>
